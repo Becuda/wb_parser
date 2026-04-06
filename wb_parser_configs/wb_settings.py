7bdc23e62a7c9c978481c3479fa70ca101fc7655
Becuda <cube4004@gmail.com>
2026-04-06 15:04:41 +0300

    add README and requirements files; update settings class for clarity

--- wb_parser_configs/wb_settings.py
+++ wb_parser_configs/wb_settings.py
@@ -2,7 +2,6 @@ class Settings:
     WB_HOME_URL="https://www.wildberries.ru/"
     SEARCH_TEXT="пальто из натуральной шерсти"
 
-    #0 - infinity
     MAX_PRODUCT_FOR_PARSSING=5
 
     OUTPUT_FILE="wb_product_result.csv"
